91fb6c8b6fdea89ade7f1f5a0bdcc2a5cd18ad9f
drewski <drew.howell@stacksports.com>
2019-12-16 21:26:02 -0500

    added criteria prompts and captured responses in console

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -3,12 +3,28 @@
 function criteriaPrompt() {
     var characterLimit = prompt("How many characters?");
     console.log(characterLimit);
+
+    if (characterLimit < 8) {
+        alert("Password length must be greater than 8");
+    }
+
+    else if (characterLimit >128) {
+        alert("Password length must be less than 128");
+    }
+
+    else {
+
     var numbers = confirm("Do you want to use numbers?");
     console.log(numbers);
     var lowercase = confirm("Do you want to use lowercase letters?");
     console.log(lowercase);
     var uppercase = confirm("Do you want to use uppercase letters?");
     console.log(uppercase);
+
+    }
 }
 
 //Create password based off of criteria selected
+if (characterLimit < 8) {
+    alert("Password length must be greater than 8");
+}
