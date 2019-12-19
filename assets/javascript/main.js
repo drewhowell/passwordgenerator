25abc0a04e73d73f2dd4dfd3d55f4fdc8ad7730d
drewski <drew.howell@stacksports.com>
2019-12-19 09:44:23 -0500

    generated password and displayed on screen wahoozle

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -20,11 +20,55 @@ function criteriaPrompt() {
     console.log(lowercase);
     var uppercase = confirm("Do you want to use uppercase letters?");
     console.log(uppercase);
+    var specialCharacters = confirm("Do you want to use special characters?");
+    console.log(specialCharacters);
+
+    generate(characterLimit,numbers,lowercase,uppercase,specialCharacters);
 
     }
 
 }
 
+//Generate password based on criteria and display on page
 
+function generate(characterLimit,numbers,lowercase,uppercase,specialCharacters) {
+
+    let complexity = characterLimit;
+
+    let values = "";
+
+    let numbersString = "1234567890";
+
+    let lowercaseString = "abcdefghijklmnopqrstuvwxyz";
+
+    let uppercaseString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    let specialCharactersString = "!@#$%^&*()_+";
+
+    let password = "";
+
+    if (numbers === true) {
+        values += numbersString;
+    }
+
+    if (lowercase === true) {
+        values += lowercaseString;
+    }
+
+    if (uppercase === true) {
+        values += uppercaseString;
+    }
+
+    if (specialCharacters === true) {
+        values += specialCharactersString;
+    }
+
+for(var i = 0; i <= complexity; i++){
+    password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length -1)));
+    }
+    console.log(password);
+
+    document.getElementById("display").textContent = password;
+}
 
 
